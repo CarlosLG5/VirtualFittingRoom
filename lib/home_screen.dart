891fc0925cbef0273729fdde7ce48d1fde2fa29d
JakobBarrows <jakob.barrows03@gmail.com>
2025-03-24 14:20:52 -0400

    Progress on DL integration
    creating the interpreter

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'contact.dart'; //imports our contact screen
 import 'package:image_picker/image_picker.dart'; //allows for use of images
 import 'dart:io'; //allows for input/output file use
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:image/image.dart' as img;
 
 class HomeScreen extends StatefulWidget{
   const HomeScreen({Key? key}) : super(key : key);
@@ -17,13 +19,30 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   final TextEditingController _weightController = TextEditingController(); //weight input
   File? _image;
   final ImagePicker _picker = ImagePicker(); //image input
+  Interpreter? _interpreter; //interpreter for our model
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
+    loadModel();
   }
 
+  //load model, return the result
+  void loadModel() async{
+    try{
+      _interpreter = await Interpreter.fromAsset('assets/VFRmodel.tflite');
+      print("model loaded successfully");
+      var inputTensors = _interpreter!.getInputTensors();
+      for (var i = 0; i < inputTensors.length; i++) {
+        print("Input tensor $i: shape: ${inputTensors[i].shape}, type: ${inputTensors[i].type}");
+      }
+    }catch(e){
+      print("failed to load model");
+    }
+  }
+
+  
   //image importing from user
   Future<void> _pickImage() async{
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
@@ -34,6 +53,53 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
     }
   }
 
+  //image camera utility
+  Future<void> _captureImageFromCamera() async{
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+    if(pickedFile != null){
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  //preprocess the image
+  Future<img.Image?> preprocessImage(File image) async{
+    img.Image imageTemp = img.decodeImage(image.readAsBytesSync())!;
+    img.Image resizedImg = img.copyResize(imageTemp, width: 220, height: 220);//change values as needed
+    return resizedImg;
+  }
+
+  //putting the model to use hopefully
+  void predictSuitSize() async{
+    if(_image != null && _heightController.text.isNotEmpty && _weightController.text.isNotEmpty){
+      double height = double.parse(_heightController.text); //parse to get height
+      double weight = double.parse(_weightController.text); //parse to get weight
+      var imageInput = await preprocessImage(_image!); //image use
+      
+      if(imageInput != null){
+        List<double> imageList = imageInput.data.map((pixel) => (img.getRed(pixel) + img.getGreen(pixel) + img.getBlue(pixel)) / 3.0 / 255.0).toList();
+        var input = [height, weight] + imageList;
+        var output = List.filled(1, 0.0);
+
+        _interpreter?.run(input, output); //when button is pressed to display output, this gives an error. states it failed prerequisite checks.
+        String result = output[0].toString();
+
+        showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+            title: Text("Predicted Suit Size"),
+            content: Text(result),
+          )
+        );
+      }else{
+        print("image is null");
+      }
+    }else{
+      print("image or else is null");
+    }
+  }
+
   @override
   void dispose(){
     _tabController.dispose();
@@ -41,6 +107,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
     _weightController.dispose();
     super.dispose();
   }
+
   //create widget for info input
   Widget _buildFittingRoomTab(){
     return SingleChildScrollView(
@@ -70,10 +137,9 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
           ),
           SizedBox(height: 10),
           _image != null ? Image.file(_image!) : Text("Image not selected"),
-          ElevatedButton(
-            onPressed: _pickImage,
-            child: Text('Pick Image'),
-          ),
+          ElevatedButton(onPressed: _pickImage,child: Text('Pick Image'),),
+          ElevatedButton(onPressed: _captureImageFromCamera , child: Text('Camera'),),
+          ElevatedButton(onPressed: predictSuitSize, child: Text("Predict Size"),) //runs the model
         ],
       ),
     );
@@ -82,6 +148,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 
   }
 
+  //appbar at the top
   @override
   Widget build(BuildContext context){
     return Scaffold(
