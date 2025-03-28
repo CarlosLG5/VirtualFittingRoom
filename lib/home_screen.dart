80a013da241eadfeb532e90ac5724357fc3da3d1
JakobBarrows <jakob.barrows03@gmail.com>
2025-03-27 21:13:03 -0400

    Contact Screen added
    added the contact screen widget code to Carlos's progress on iOS

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'contact.dart'; //imports our contact screen
 import 'package:image_picker/image_picker.dart'; //allows for use of images
 import 'dart:io'; //allows for input/output file use
+import 'package:camera/camera.dart';
+
+late List<CameraDescription> _cameras;
+
 
 class HomeScreen extends StatefulWidget{
   const HomeScreen({Key? key}) : super(key : key);
@@ -17,6 +21,9 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   final TextEditingController _weightController = TextEditingController(); //weight input
   File? _image;
   final ImagePicker _picker = ImagePicker(); //image input
+
+String _suitSize=""; //store suit size
+ bool _dataEntered = false; //track if data entered
 
   @override
   void initState() {
@@ -34,6 +41,27 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
     }
   }
 
+  //image camera utility
+  Future<void> _captureImageFromCamera() async{
+    final PickedFile = await _picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = File(PickedFile!.path);
+    });
+
+  }
+
+  //method to calculate suit size(for algorithm)
+  String _calculateSuitSize(double height, double weight){
+    if(height<160){
+      return 'Small';
+    }else if (height>=160&&height<175){
+      return 'Medium';
+    }else{
+      return 'Large';
+    }
+  }
+
   @override
   void dispose(){
     _tabController.dispose();
@@ -42,49 +70,143 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
     super.dispose();
   }
   //create widget for info input
-  Widget _buildFittingRoomTab(){
-    return SingleChildScrollView(
-      child: Column(
-        children: [
+  Widget _buildFittingRoomTab() {
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+      if(!_dataEntered) ...[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            controller: _heightController,
+            decoration: InputDecoration(
+              labelText: 'Enter your height in inches',
+              border: OutlineInputBorder(),
+            ),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            controller: _weightController,
+            decoration: InputDecoration(
+              labelText: 'Enter your weight in lbs',
+              border: OutlineInputBorder(),
+            ),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+
+        // Body Type Selection Buttons
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _heightController,
-              decoration: InputDecoration(
-                labelText: 'Enter your height in inches',
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.number,
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
+              children: [
+                Text(
+                  "Select Body Type:",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                Wrap(
+                  spacing: 8,
+                  children: ['Skinny', 'Average', 'Athletic', 'Larger']
+                      .map((type) => ChoiceChip(
+                            label: Text(type),
+                            selected: selectedBodyType == type,
+                            onSelected: (selected) {
+                              setState(() {
+                                selectedBodyType = type;
+                              });
+                            },
+                          ))
+                      .toList(),
+                ),
+              ],
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _weightController,
-              decoration: InputDecoration(
-                labelText: 'Enter your weight in lbs',
-                border: OutlineInputBorder(),
+
+
+
+        SizedBox(height: 10),
+        _image != null 
+          ? Image.file(_image!) 
+          : Text("Image not selected"),
+        ElevatedButton(
+          onPressed: _pickImage,
+          child: Text('Pick Image'),
+        ),
+        ElevatedButton(
+          onPressed: _captureImageFromCamera,
+          child: Text('Camera'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            double height = double.tryParse(_heightController.text) ?? 0;
+            double weight = double.tryParse(_weightController.text) ?? 0;
+            String calculatedSuitSize = _calculateSuitSize(height, weight);
+            setState(() {
+              _suitSize = calculatedSuitSize;
+              _dataEntered = true;
+            });
+          },
+          child: Text('Enter Data'),
+        ),
+      ],
+
+        // When data is entered, display the results
+        if (_dataEntered) 
+          Column(
+            children: [
+              SizedBox(height: 10),
+              Text(
+                'Height: ${_heightController.text} inches',
+                style: TextStyle(fontSize: 16),
               ),
-              keyboardType: TextInputType.number,
-            ),
+              Text(
+                'Weight: ${_weightController.text} lbs',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 10),
+              _suitSize.isNotEmpty
+                  ? Text(
+                      'Predicted Suit Size: $_suitSize',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )
+                  : SizedBox.shrink(),
+              SizedBox(height: 10),
+              _image != null
+                  ? Image.file(
+                      _image!,
+                      width: 150, // Make image smaller
+                      height: 150, // Make image smaller
+                      fit: BoxFit.cover,
+                    )
+                  : Text("No image selected"),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _dataEntered = false;
+                    _heightController.clear();
+                    _weightController.clear();
+                    _suitSize = "";
+                    _image = null;
+                  });
+                },
+                child: Text('Enter New Data'),
+              ),
+            ],
           ),
-          SizedBox(height: 10),
-          _image != null ? Image.file(_image!) : Text("Image not selected"),
-          ElevatedButton(
-            onPressed: _pickImage,
-            child: Text('Pick Image'),
-          ),
-        ],
-      ),
-    );
-
-
-
-  }
-
+      ],
+    ),
+  );
+}
+  // Buttons for body type
+  String selectedBodyType = 'None selected';
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      
       appBar: AppBar(
         title: const Text(
           'Virtual Fitting Room',
@@ -112,6 +234,9 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
           ContactScreen(),
         ],
       ),
+      
+
+    
       backgroundColor: Colors.white,
     );
   }
